b145402f847893ef44b5791ed566d35827f0366a
ruiruigirl <41535602+ruiruigirl@users.noreply.github.com>
2019-01-18 23:10:16 +0800

    新增内容 ：并发和Effective_Java 实例

---
+++ src/java/Effective_java/第六章_枚举和注解/用enum代替int常量.java
@@ -0,0 +1,14 @@
+package Effective_java.第六章_枚举和注解;
+
+/**
+ * @program: java_
+ * @description:
+ * @author: Mr.Zhou
+ * @create: 2019-01-14 21:00
+ **/
+
+public class 用enum代替int常量 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/java/并发/AttempLocking.java
@@ -0,0 +1,55 @@
+package 并发;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @program: java_
+ * @description:
+ * @author: Mr.Zhou
+ * @create: 2019-01-18 22:30
+ **/
+
+public class AttempLocking {
+    private ReentrantLock lock = new ReentrantLock();
+    public void untimed() {
+        boolean captured = lock.tryLock();
+        try{
+            System.out.println("tryLock(): " + captured);
+        } finally {
+            if (captured)
+                lock.unlock();
+        }
+    }
+
+    public void timed() {
+        boolean captured = false;
+        try {
+            captured = lock.tryLock(2 , TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw  new RuntimeException(e);
+        }
+        try{
+            System.out.println("tryLock(2 , TimeUnit.SECONDS): " + captured);
+        } finally {
+            if (captured)
+                lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        final AttempLocking al = new AttempLocking();
+        al.untimed();
+        al.timed();
+        new Thread(){
+            { setDaemon(true);}
+            public void run() {
+                al.lock.lock();
+                System.out.println("acquired");
+            }
+        }.start();
+        Thread.yield();
+        al.untimed();
+        al.timed();
+    }
+}
