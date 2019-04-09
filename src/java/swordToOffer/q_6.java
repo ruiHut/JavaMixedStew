6006278d9b2d56f41a3a3e6494c5311d5748eaee
mac_for_hut <1147681296@qq.com>
2019-04-10 00:18:44 +0800

    feat(src/swordToOffer): add method addToTail to singelListNode

--- src/java/swordToOffer/q_6.java
+++ src/java/swordToOffer/q_6.java
@@ -2,6 +2,8 @@ package swordToOffer;
 
 // 基础知识之链表
 
+import com.sun.xml.internal.bind.v2.TODO;
+
 //数据结构-单向链表
 // struct ListNode
 //  int value;
@@ -9,18 +11,18 @@ package swordToOffer;
 public class q_6 {
     public static void main(String[] args) {
         SingleListNode listNode = new SingleListNode(0);
-//        listNode.addToTail(1);
+        listNode.addToTail(1);
 
         while (listNode != null) {
             System.out.println(listNode.getValue());
-            listNode = listNode.pNext;
+            listNode = listNode.getPNext();
         }
     }
 }
 
 class SingleListNode<T> {
     private T value;
-    public SingleListNode<T> pNext;
+    private SingleListNode<T> pNext;
 
     SingleListNode(T value){
         this.value = value;
@@ -35,24 +37,27 @@ class SingleListNode<T> {
         return value;
     }
 
+    public SingleListNode getPNext() {
+        return pNext;
+    }
+
     public void setValue(T value) {
         this.value = value;
     }
 
-    // TODO 有待实现一个添加在链表末尾添加新链表的方法
+    public void addToTail(T value) {
+        SingleListNode tailNode = getTailNode();
 
-//    public void addToTail(T value) {
-//        SingleListNode tailNode = getTailNode();
-//
-//        SingleListNode newNode = new SingleListNode(value);
-//        tailNode.pNext = newNode;
-//    }
-//
-//    private SingleListNode getTailNode() {
-//        SingleListNode pHead = SingleListNode.this;
-//        while(pNext != null)
-//            pNext = pNext.pNext;
-//
-//        return pNext;
-//    }
+        SingleListNode newNode = new SingleListNode(value);
+        tailNode.pNext = newNode;
+    }
+
+    private SingleListNode getTailNode() {
+        SingleListNode pHead = SingleListNode.this;
+
+        while(pHead.pNext != null)
+            pHead = pHead.pNext;
+
+        return pHead;
+    }
 }
