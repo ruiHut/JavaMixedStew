f76bbf947f708f2105bc6e4090243ae1d4d25802
mac_for_hut <1147681296@qq.com>
2019-04-10 21:23:27 +0800

    feat(src/sword): add remove first node function

--- src/java/swordToOffer/q_6.java
+++ src/java/swordToOffer/q_6.java
@@ -12,19 +12,50 @@ public class q_6 {
     public static void main(String[] args) {
         SingleListNode listNode = new SingleListNode(0);
         listNode.addToTail(1);
+        listNode.addToTail(2);
+        listNode.addToTail(3);
+        listNode.addToTail(4);
 
+        removeFirst(listNode, 0);
+        removeFirst(listNode, 3);
         while (listNode != null) {
             System.out.println(listNode.getValue());
-            listNode = listNode.getPNext();
+            listNode = listNode.pNext;
+        }
+    }
+
+    public static void removeFirst(SingleListNode pHead, int value) {
+
+        // 链表头为需删除节点
+        if (pHead.getValue() == value) {
+            SingleListNode pDelNode = pHead;
+            pHead = pDelNode.pNext;
+            pDelNode = null;
+            return;
+        }
+
+        if (pHead.pNext == null)
+            return;
+
+        SingleListNode preNode = pHead;
+        while (preNode.pNext != null) {
+            SingleListNode delNode = preNode.pNext;
+            if (delNode.getValue() == value) {
+                preNode.pNext = delNode.pNext;
+                delNode.pNext = null;
+                return;
+            } else {
+                preNode = preNode.pNext;
+            }
         }
     }
 }
 
-class SingleListNode<T> {
-    private T value;
-    private SingleListNode<T> pNext;
+class SingleListNode {
+    private int value;
+    public SingleListNode pNext;
 
-    SingleListNode(T value){
+    SingleListNode(int value){
         this.value = value;
         pNext = null;
     }
@@ -33,19 +64,15 @@ class SingleListNode<T> {
         return pNext == null;
     }
 
-    public T getValue() {
+    public int getValue() {
         return value;
     }
 
-    public SingleListNode getPNext() {
-        return pNext;
-    }
-
-    public void setValue(T value) {
+    public void setValue(int value) {
         this.value = value;
     }
 
-    public void addToTail(T value) {
+    public void addToTail(int value) {
         SingleListNode tailNode = getTailNode();
 
         SingleListNode newNode = new SingleListNode(value);
