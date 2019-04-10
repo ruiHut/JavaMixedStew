6dba497529fa5a7c262f30731bfa3df03dee1243
mac_for_hut <1147681296@qq.com>
2019-04-10 21:48:08 +0800

    feat(src/swordToOffer): gc stupit LinkedList realize and add some nomal LinkedList

--- src/java/swordToOffer/q_6.java
+++ src/java/swordToOffer/q_6.java
@@ -10,81 +10,59 @@ import com.sun.xml.internal.bind.v2.TODO;
 //  ListNode* m_pNext;
 public class q_6 {
     public static void main(String[] args) {
-        SingleListNode listNode = new SingleListNode(0);
-        listNode.addToTail(1);
-        listNode.addToTail(2);
-        listNode.addToTail(3);
-        listNode.addToTail(4);
 
-        removeFirst(listNode, 0);
-        removeFirst(listNode, 3);
-        while (listNode != null) {
-            System.out.println(listNode.getValue());
-            listNode = listNode.pNext;
-        }
     }
 
-    public static void removeFirst(SingleListNode pHead, int value) {
+}
 
-        // 链表头为需删除节点
-        if (pHead.getValue() == value) {
-            SingleListNode pDelNode = pHead;
-            pHead = pDelNode.pNext;
-            pDelNode = null;
-            return;
-        }
+// 环形链表
+class RingList {
+    // 环形链表对象熟悉
+    int length;
+    Node head;
+    Node last;
 
-        if (pHead.pNext == null)
-            return;
 
-        SingleListNode preNode = pHead;
-        while (preNode.pNext != null) {
-            SingleListNode delNode = preNode.pNext;
-            if (delNode.getValue() == value) {
-                preNode.pNext = delNode.pNext;
-                delNode.pNext = null;
-                return;
-            } else {
-                preNode = preNode.pNext;
-            }
+    // 链表基本数据结构
+    class Node {
+        int value;
+        Node nextNode;
+
+        Node (int value) {
+            this.value = value;
         }
     }
 }
 
-class SingleListNode {
-    private int value;
-    public SingleListNode pNext;
+// 单向链表
+class singleLinkedList {
+    int length;
+    Node head;
 
-    SingleListNode(int value){
-        this.value = value;
-        pNext = null;
-    }
+    class Node {
+        int value;
+        Node nextNode;
 
-    public boolean isEmpty() {
-        return pNext == null;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
-
-    public void addToTail(int value) {
-        SingleListNode tailNode = getTailNode();
-
-        SingleListNode newNode = new SingleListNode(value);
-        tailNode.pNext = newNode;
-    }
-
-    private SingleListNode getTailNode() {
-        SingleListNode pHead = SingleListNode.this;
-
-        while(pHead.pNext != null)
-            pHead = pHead.pNext;
-
-        return pHead;
+        Node (int value) {
+            this.value = value;
+        }
     }
 }
+
+// 双向链表
+class DoubleLinkedList {
+    int length;
+    Node head;
+
+    class Node {
+        int value;
+        Node preNode;
+        Node nextNode;
+
+        Node(int value) {
+
+        }
+    }
+}
+
+// TODO 有待补充其他链表形式的数据结构
