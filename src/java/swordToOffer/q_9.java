a755840d761c8e2bfd57a666311e0f633d5ff8b8
mac_for_hut <1147681296@qq.com>
2019-04-15 14:40:04 +0800

    fix(java/swordToOffer): fix solution of double stack relized queue

--- src/java/swordToOffer/q_9.java
+++ src/java/swordToOffer/q_9.java
@@ -3,50 +3,67 @@ package swordToOffer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 // 用栈实现队列
 public class q_9 {
     public static void main(String[] args) {
         Queue q = new Queue();
-        q.appendTail(1);
-        q.appendTail(2);
-        System.out.println(q.deleteHead());
-        q.deleteHead();
-        q.deleteHead();
+        q.push(1);
+        q.push(2);
+        System.out.println(q.pop());
+
+        ListStack s = new ListStack();
+        s.push(1);
+        s.push(2);
+        System.out.println(s.pop());
     }
 }
 
 // 链表实现栈
 // TODO 对 list添加
 // TODO 对其增加并发控制
-class Stack {
+class ListStack {
     private static List<Integer> l = new ArrayList();
 
     public int pop() {
-        if (l.size() > 0)
-            throw new  IllegalArgumentException("无更多数据！");
-        int result = l.get(l.size() - 1);
-        l.remove(l.size() - 1);
-        return result;
+        if (l.size() == 0)
+            throw new RuntimeException("无更多数据！");
+
+        return l.remove(l.size() - 1);
     }
 
     public void push(int value) {
         l.add(value);
     }
+
+    public boolean empty() {
+        return l.size() == 0;
+    }
 }
 
 // 两个栈实现队列
 class Queue {
-    private static Stack sTail = new Stack();
-    private static Stack sHead = new Stack();
+    private static Stack<Integer> stack1 = new Stack<>();
+    private static Stack<Integer> stack2 = new Stack<>();
 
-    public void appendTail(int value) {
-        sTail.push(value);
+    // 从队尾添加元素
+    public void push(int value) {
+        stack1.push(value);
     }
 
-    public int deleteHead() {
-        sHead.push(sTail.pop());
-        return sHead.pop();
-    }
+    // 从队列头部弹出元素
+    public int pop() {
+        if (stack1.empty() && stack2.empty())
+            throw new RuntimeException("Queue is empty");
 
+        if (stack2.empty()) {
+            // 将 stack1 中保留数据，推到 stack2
+            while (!stack1.empty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+
+        return stack2.pop();
+    }
 }
